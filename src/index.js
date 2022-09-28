1ed4764819ba02be9c2f0a95bf87e01300bec7e9
Amadi Y <absnacks17@gmail.com>
2022-09-28 10:33:37 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Connected!!!");
+const container = document.querySelector(".container")
+
+container.innerHTML = `
+    <header id="header">
+        <h1>What's the weather like</h1>
+        <input type="text" placeholder="Your city here"/>
+        <button>Submit</button>
+    </header>
+`;
